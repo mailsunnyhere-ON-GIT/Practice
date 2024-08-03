95651cd5e72f62122a8625433b84f3ce0c9f6dc9
Your Name <mailsunnyhere@gmail.com>
2024-08-04 00:38:04 +0530

    adding hero feature

--- react/four/App.jsx
+++ react/four/App.jsx
@@ -1,19 +1,23 @@
-import Header from "./components/Header"
-import "./App.css"
-import SearchBar from "./components/SearchBar"
-import SelectMenu from "./components/SelectMenu"
-export default function App()
-{
-   return(
-   <>
-    <Header/>
-    <main>
-    <div className="search-filter-container">
-      <SearchBar/>
-      <SelectMenu/>
-    </div>
-    </main>
-    
+import Header from './components/Header'
+import SearchBar from './components/SearchBar'
+import SelectMenu from './components/SelectMenu'
+import CountriesList from './components/CountriesList'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="search-filter-container">
+          <SearchBar />
+          <SelectMenu />
+        </div>
+        <CountriesList />
+      </main>
     </>
-   )
+  )
 }
+
+export default App
